594f3143e83aecc9df7825c4065d07a19519edf2
YagoNep <88169818+YagoNep@users.noreply.github.com>
2022-07-06 22:01:50 -0300

    Add files via upload

--- site/js/main.js
+++ site/js/main.js
@@ -1,4 +1,5 @@
 document.querySelector('#add').addEventListener("click", teste);
+document.querySelector('#teste4').addEventListener("click", teste2);
 
 function teste(){
     fetch('/user')
@@ -15,3 +16,67 @@ function criarLinha(email){
     teste.innerText = teste2;
     console.log(email);
 }
+
+function teste2(){
+    fetch('/imagem')
+    .then((res) => res.json())
+    .then((res) => {
+        for(foto of res){
+        criarImagem(foto.foto);
+}})
+}
+function criarImagem(resposta){
+    var teste3 = document.getElementById('teste3');
+    teste3.src=resposta;
+}
+
+function populatesUfs() {
+    const ufSelect = document.querySelector("select[name=uf]")
+    
+    fetch("https://servicodados.ibge.gov.br/api/v1/localidades/estados")
+    .then( res => res.json() )
+    .then( states => {
+        
+        for ( const state of states ) {
+            ufSelect.innerHTML += `<option value='${state.id}'>${state.nome}</option>`
+        }
+        
+    } )
+}
+
+populatesUfs()
+
+
+function getCities(event) {
+    const citySelect = document.querySelector("[name=city]")
+    const stateInput = document.querySelector("[name=state]")
+
+    const ufValue = event.target.value
+
+
+    const indexOfSelectedState = event.target.selectedIndex
+    stateInput.value = event.target.options[indexOfSelectedState].text
+
+
+    const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios`
+
+    citySelect.innerHTML = "<option value>Selecione a Cidade</option>"
+    citySelect.disabled = true
+
+    fetch(url)
+    .then( res => res.json() )
+    .then( cities => {
+        
+        for ( const city of cities ) {
+            citySelect.innerHTML += `<option value='${city.nome}'>${city.nome}</option>`
+        }
+
+        citySelect.disabled = false
+    } )
+
+
+}
+
+document
+    .querySelector("select[name=uf]")
+    .addEventListener("change", getCities)
