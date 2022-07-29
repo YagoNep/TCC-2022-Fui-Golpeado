f0726eee7c9109273ef50563e5bc2f47b8700606
YagoNep <88169818+YagoNep@users.noreply.github.com>
2022-07-28 22:27:15 -0300

    Add files via upload

--- site/js/inicio.js
+++ site/js/inicio.js
@@ -1,3 +1,5 @@
+let auxpagina = 0;
+
 function carregarPerfil() {
     fetch('/user')
         .then((res) => res.json())
@@ -15,33 +17,46 @@ function mostrarPerfil(res) {
 
 carregarPerfil();
 
-function carregarnoticias(){
+// function iterarRelatos(){
+//     fetch('/relatos')
+//     .then((res) => res.json())
+//     return await res;
+// }
+
+function carregarrelatos(){ /////dar um jeito de arrumar essa merda mostrar só uns 10 por vez
+
     fetch('/relatos')
     .then((res) => res.json())
     .then((res) => {
-        console.log(res)
-            for(const relato of res){
-                criarcard(relato.ID_Relato, relato.Titulo, relato.Descricao);
+        var aux = res;
+        var aux1 = 0;
+        
+            for(const relato of aux){
+                for (let i = aux1; i < 20; i++){
+                    criarcard(relato.ID_Relato, relato.Titulo, relato.Descricao);
             }
-        }
-    )}
+            aux1+20;
+    }}
+    )
+    
+}
 
 function criarcard(id, titulo, descricao){
 var card = document.createElement("div");
 card.className = "col-lg-6 col-md-6 mb-4";
-card.nodeValue = id;
+card.setAttribute("value", id);
 var card1 = document.createElement("div");
-card1.className = "card bg-dark text-light";
+card1.className = "card bg-dark text-light h-100 p-5";
 var card2 = document.createElement("div");
 card2.className = "card-body";
-var titulo1 = document.createElement("h5");
+var titulo1 = document.createElement("h4");
 titulo1.textContent = titulo;
 titulo1.className = "card-title";
 var descricao1 = document.createElement("p");
-descricao1.textContent = descricao;
+descricao1.textContent = descricao.substring(0,175) + "...";
 descricao1.className = "card-text";
 var botao = document.createElement("a");
-botao.className = "btn btn-primary";
+botao.className = "btn btn-primary mt-2";
 botao.textContent = "Continuar Lendo";
 
 card2.appendChild(titulo1);
@@ -50,8 +65,7 @@ card2.appendChild(botao);
 card1.appendChild(card2);
 card.appendChild(card1);
 
-console.log(card);
 document.getElementById("teste").appendChild(card)
 }
 
-carregarnoticias();
+carregarrelatos();
